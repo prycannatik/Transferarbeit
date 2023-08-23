5c24c3f5936aa10a5bddafc263b900667f85cc48
Samuel Haab <samuel.haab@edu.teko.ch>
2023-08-23 18:35:02 +0200

    initial

---
+++ app/src/main/java/grafik/App.java
@@ -0,0 +1,32 @@
+package grafik;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello World!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+
+        Scene scene = new Scene(root, 300, 200);
+
+        primaryStage.setTitle("JavaFX HelloWorld");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+  
+}
+
+
